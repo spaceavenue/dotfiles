d1414d7a815a44b769524c8445f3e84bdb303d46
spaceavenue <spaceavenue@proton.me>
2026-05-04 14:19:06 +0530

    wayland apps and stuff

---
+++ .config/nvim/lua/user/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.backup = false
+opt.swapfile = false
+opt.clipboard = "unnamedplus"
+opt.number = true
+opt.relativenumber = false
+opt.showmode = false
+opt.showtabline = 2
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop= 4
+opt.shiftwidth= 4
+opt.smartindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.conceallevel = 0
+opt.cursorline = true --highlight current line
+opt.background = "dark"
