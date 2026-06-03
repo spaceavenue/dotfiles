4f8f897ef5dab48e0f59f3e8b7bcac0479d998ac
spaceavenue <spaceavenue@proton.me>
2026-06-03 18:47:48 +0530

    hey maybe stop moving stuff around w/o documenting it

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,3 +1,32 @@
+# load the colors module
+autoload -Uz colors && colors
+
+# prompt
+PROMPT='[%B%F{9}%n%f%b%F{18}@%f%B%F{19}%m%f%b %F{18}%1~%f ] '
+
+# opts for: autocomplete, menu nav and history
+setopt auto_cd always_to_end menu_complete extended_glob nomatch interactive_comments share_history
+
+# completions module
+autoload -Uz compinit && compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-$ZSH_VERSION
+
+# menu style and completion cache
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+
+# i forgor what this does
+_comp_options+=(globdots)
+
+# to select menu entries
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+
+# for highlighting selections
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+# everything else(tm)
 source $ZDOTDIR/variables.zsh
 source $ZDOTDIR/plugins.zsh
 source $ZDOTDIR/alias.zsh
--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -1,30 +1,14 @@
-export PATH=$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/opt
+# update PATH to include local bin dir
+export PATH=$HOME/.local/bin:$PATH 
+
+# history settings
+HISTFILE="$XDG_STATE_HOME"/zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
 
 # put rust libs in better directory
 export RUSTUP_HOME=$HOME/.local/lib/rustup
 export CARGO_HOME=$HOME/.local/lib/cargo
-
-# set JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/default-runtime/bin
-
-# update PATH
-export PATH=$PATH:$JAVA_HOME:$RUSTUP_HOME:$CARGO_HOME
-
-# for firefox (might be unnecessary?)
-export MOZ_ENABLE_WAYLAND=1
-
-# for qt apps
-export QT_QPA_PLATFORM=wayland
-export QT_QPA_PLATFORMTHEME=qt5ct:qt6ct
-
-# apply bat theme
-export BAT_THEME="Catppuccin Mocha"
-
-# default wineprefix path
-export WINEPREFIX="$XDG_DATA_HOME"/wineprefixes/default
-
-# prevent .java directory from being created
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 
 # wget config path
 export WGETRC="$XDG_CONFIG_HOME"/wgetrc
@@ -35,39 +19,9 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 # gnupg key storage
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 
-# history file
-HISTFILE="$XDG_STATE_HOME"/zsh_history
-HISTSIZE=1000
-SAVEHIST=10000
-
 # mariadb history
 export MYSQL_HISTFILE="$XDG_STATE_HOME"/mariadb_history
 
-# set editor to nvim
+# set editor as nvim
 export EDITOR='/usr/bin/nvim'
 
-# load the colors module
-autoload -Uz colors && colors
-
-# prompt
-PROMPT='[%B%F{9}%n%f%b%F{18}@%f%B%F{19}%m%f%b %F{18}%1~%f ] '
-
-# autocomplete and directory nav options
-setopt auto_cd always_to_end menu_complete extended_glob nomatch interactive_comments share_history
-
-autoload -Uz compinit && compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-$ZSH_VERSION
-
-# style of menu
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-
-_comp_options+=(globdots)
-
-# for being able to select the menu entries
-autoload -Uz up-line-or-beginning-search
-autoload -Uz down-line-or-beginning-search
-
-# for highlighting the selections
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
