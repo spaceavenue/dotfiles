0b8df6c6ea00eb143c7319187549a7c6581f188a
spaceavenue <spaceavenue@proton.me>
2026-07-20 13:29:27 +0530

    better(?) configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,32 +1,30 @@
-if status is-login
-	fish_add_path "$HOME"/.local/bin/prog "$HOME"/.local/bin/
-	set -gx XDG_CONFIG_HOME "$HOME"/.config
-	set -gx XDG_CACHE_HOME  "$HOME"/.cache
-	set -gx XDG_DATA_HOME   "$HOME"/.local/share
-	set -gx XDG_STATE_HOME  "$HOME"/.local/state
-end
+set -gx XDG_CONFIG_HOME "$HOME"/.config
+set -gx XDG_CACHE_HOME  "$HOME"/.cache
+set -gx XDG_DATA_HOME   "$HOME"/.local/share
+set -gx XDG_STATE_HOME  "$HOME"/.local/state
+set -gx FISH_LOGIN_ENV_LOADED 1
 
-if status is-interactive
-    set -g fish_greeting
-	set -g fish_history_derivative true
-	
-	set -gx WGETRC "$XDG_CONFIG_HOME"/wgetrc
-	set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
-	
-	set -gx RIPGREP_CONFIG_PATH "$XDG_CONFIG_HOME"/ripgrep/config
+not status is-interactive && return
 
-	set -gx GNUPGHOME "$XDG_DATA_HOME"/gnupg
+set -g fish_greeting
+set -g fish_history_derivative true
 
-	set -gx MYSQL_HISTFILE "$XDG_STATE_HOME"/mariadb_history
+set -gx WGETRC "$XDG_CONFIG_HOME"/wgetrc
+set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
+set -gx RIPGREP_CONFIG_PATH "$XDG_CONFIG_HOME"/ripgrep/config
 
-	set -gx EDITOR "/usr/bin/nvim"
-	set -gx VISUAL $EDITOR
+set -gx GNUPGHOME "$XDG_DATA_HOME"/gnupg
 
-	set -gx PAGER "bat -p --paging=always"
-	set -gx GIT_PAGER $PAGER
+set -gx MYSQL_HISTFILE "$XDG_STATE_HOME"/mariadb_history
 
-	# bat gets confused because aurutils actually opens a directory.
-	# could be set to nvim? maybe?
-	set -gx AUR_PAGER yazi
-	set -gx AUR_PACMAN_AUTH run0
-end
+set -gx EDITOR "/usr/bin/nvim"
+set -gx VISUAL $EDITOR
+
+set -gx PAGER "bat -p --paging=always"
+set -gx GIT_PAGER $PAGER
+
+# bat gets confused because aurutils actually opens a directory.
+# could be set to nvim? maybe?
+set -gx AUR_PAGER yazi
+set -gx AUR_PACMAN_AUTH run0
+
