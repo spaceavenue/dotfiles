9c7fb00f4b7d60a297ca29f5b7640e65c728830b
spaceavenue <spaceavenue@proton.me>
2026-08-06 22:39:52 +0530

    update xdg dirs, screenshot save path, nvim config, add wtype tracking, finer width and height adjustments in niri

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -5,18 +5,32 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- increase and decrease indent
 keymap("v", "<", "<gv^", opts)
 keymap("v", ">", ">gv^", opts)
 
-keymap("v", "p", '"_dP', opts)
-
+-- undo
 keymap("n", "<C-u>", ":u<CR>", opts)
 
+-- delete words backwards in insert mode
 vim.keymap.set('i', '<C-BS>', '<C-w>', opts)
 vim.keymap.set('i', '<C-H>', '<C-w>', opts)
 
+-- delete words forwards in insert mode
 vim.keymap.set('i', '<C-Delete>', '<C-o>dw', opts)
 
+-- switch window focus
+vim.keymap.set('n', '<S-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<S-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<S-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<S-l>', '<C-w>l', opts)
+
+vim.keymap.set('n', '<C-Left>',  '<C-w>h', opts)
+vim.keymap.set('n', '<C-Right>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-Down>',  '<C-w>j', opts)
+vim.keymap.set('n', '<C-Up>',    '<C-w>k', opts)
+
+-- insert fish function boilerplate
 vim.keymap.set("i", "<C-f>", function()
   vim.snippet.expand([[
 function ${1:func_name} --description '${2}'
